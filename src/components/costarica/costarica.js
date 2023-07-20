ad7da387f14d475b1166ac0ceaad9ac9b286f1fa
Sergej Burusas <Sergej_Burusas@mail.ru>
2023-07-20 23:09:47 +0300

    add new files1

---
+++ src/components/costarica/costarica.js
@@ -0,0 +1,1 @@
+import './costarica.scss';
--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
+import '@fortawesome/fontawesome-free/js/all';
 import './index.html';
 import './index.scss';
+import "./container.scss";
+import './components/typography/typography.scss';
+import './components/navigation/navigation';
+import './components/header/header';
+import './components/button/button';
+import './components/promo/promo';
+import './components/japan/japan';
+import './components/photo/photo';
+import './components/costarica/costarica';
+import './components/brands/brands';
+import './components/nepal/nepal';
+import './components/feedback/feedback';
+import './components/footer/footer';
+import './components/social-icons/social-icons';
